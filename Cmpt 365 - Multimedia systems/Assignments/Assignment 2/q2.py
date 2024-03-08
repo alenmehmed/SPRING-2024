62384aba2be3ffbd1b377c2f78e250c02279af18
Alen Mehmedbegovic <alen.mehmedbegovic@gmail.com>
2024-03-07 17:16:09 -0800

    FInished q2 in 365 assignment 2

--- Cmpt 365 - Multimedia systems/Assignments/Assignment 2/q2.py
+++ Cmpt 365 - Multimedia systems/Assignments/Assignment 2/q2.py
@@ -34,7 +34,20 @@ for i in range(0, length*2, 2):
         hfdithered[i, j + 1] = int(hfditheredbottomleft)
         hfditheredbottomright = dither[1,1] < image[i//2, j//2]
         hfdithered[i + 1, j + 1] = int(hfditheredbottomright)
+
+print("Half tone dither: ")
 print(hfdithered)
 
 # Ordered dither 
 orderdither = np.diag(range(length))
+print("\n Ordered dither: ")
+for x in range(0, length):
+    for y in range(0, length):
+        i = x % 2
+        j = y % 2
+        if(image[x, y] > dither[i, j]): 
+            orderdither[x, y] = 1
+        else: 
+            orderdither[x, y] = 0
+
+print(orderdither)
